0706c136bf89b334a9d2b37d18174f408a273fed
anyashumlivaya <67638244+anyashumlivaya@users.noreply.github.com>
2020-07-04 22:23:20 +0300

    Add files via upload

---
+++ 3 урок 1 пункт.py
@@ -0,0 +1,10 @@
+def s_calc():
+	try:
+		x = float(input("введите 1 переменную"))
+		y = float(input("введите 2 переменную"))
+		except ValueError:
+			return
+			z = x / y
+			return z
+			print(s_calc())
+			
---
+++ 3 урок 3 задание.py
@@ -0,0 +1,11 @@
+def my_func(a, b, c):
+	sum = [a, b, c]
+	all = []
+	max_1 = max(sum)
+	total.append(max_1)
+	max_2 = max(sum)
+	total.append(max_2)
+	print(sum(total))
+	my_func(3, 8, 9)
+	print(sum(total))
+	
